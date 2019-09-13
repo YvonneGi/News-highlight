c71b27fc0c0229860bcd5020bba0303610c2b32d
YvonneGi <giryvonne@gmail.com>
2019-09-13 18:26:22 +0200

    add index.html contents and create base.html file

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_news,get_new,search_article
+from ..requests import get_news,get_new,search_new
 from .forms import ReviewForm
 from ..models import Review
 
